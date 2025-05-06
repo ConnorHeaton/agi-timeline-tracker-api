f9d3b30186d2c3dd5e155d6bd4c49d88c2f4574e
ConnorHeaton <166187303+ConnorHeaton@users.noreply.github.com>
2025-05-05 23:40:34 -0400

    Fix: Add Prediction model import to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Prediction = require('./models/Prediction');
 
 const app = express();
 
